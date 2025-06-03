841ab51d4d65d00820a9948aef437e66a176d010
Dev Poudel <poudeldev730@gmail.com>
2025-06-03 09:07:53 +0545

    initial

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+void printMessage()
+{
+    std::cout << "Hello, World!" << std::endl;
+}
+
+void main()
+{
+    printMessage();
+    // Additional functionality can be added here
+    std::cout << "This is a simple C++ program." << std::endl;
+}
